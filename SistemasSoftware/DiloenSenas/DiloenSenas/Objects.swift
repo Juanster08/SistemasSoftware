5a18767256a454d299604da646329f6cc7ac7b42
Juanster08 <88806110+Juanster08@users.noreply.github.com>
2023-10-12 14:52:20 -0600

    Update Objects.swift

--- SistemasSoftware/DiloenSenas/DiloenSenas/Objects.swift
+++ SistemasSoftware/DiloenSenas/DiloenSenas/Objects.swift
@@ -18,4 +18,37 @@ class ObjectsToggle: ObservableObject {
     @Published var sacapuntasToggle = false
     @Published var pegamentoToggle = false
     @Published var borradorPizarronToggle = false
+    
+}
+
+
+struct ObjectPickerView: View {
+    let objectName: String
+    @State private var selectedLevel: String = "Fácil"
+    
+    var body: some View {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color(red: 159/255, green: 211/255, blue: 86/255))
+                        .cornerRadius(20)
+                        .frame(width: 1000, height: 150)
+                    
+                    VStack(spacing: 10) {
+                        Spacer()
+                        Text("Objeto: \(objectName)")
+                            .font(Font.custom("JungleHope", size: 20)) 
+                        
+                        Picker("Nivel", selection: $selectedLevel) {
+                            Text("Fácil").tag("Fácil")
+                            Text("Intermedio").tag("Intermedio")
+                            Text("Avanzado").tag("Avanzado")
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        Spacer()
+                    }
+                }
+            }
+        }
 }
